aa39646787d105a5c5cddd8c0b9d48818ba26ea6
unknown <jk04032002@gmail.com>
2022-01-17 17:22:05 +0530

    playing around

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <><h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1><button className="px-2 py-5 hover:bg-transparent decoration-white bg-orange-500 text-6xl text-white hover:text-orange-500 font-bold border font-size: 1.0rem line-height: 1rem border-solid border-4 border-width: 4px border-orange-600 rounded-xl">
+      Tailwind app</button></>
+    );
+};
